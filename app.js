2e8e0c83b668dd74d470a56aeae924369068e153
arif-hstu <arif.dvm.hstu@gmail.com>
2021-02-06 02:09:54 +0600

    theme functionality adde

---
+++ app.js
@@ -0,0 +1,41 @@
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res => res.json())
+.then(data => {
+    data.forEach(country => {
+        createHtmlElement(country);
+    });
+});
+
+const createHtmlElement = country => {
+    const cardHolder = document.getElementById('card-holder');
+        
+        const card = document.createElement('div');
+        card.className = 'card';
+        const cardInfo =`
+        <img src='${country.flag}'>
+        <h3>${country.name}</h2>
+        <div class='country-details'>
+        <p>Capital: ${country.capital}</p>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        </div>
+        `;
+        card.innerHTML = cardInfo;
+
+        cardHolder.appendChild(card);
+}
+
+
+const changeTheme = () => {
+    const bodyTag = document.getElementsByTagName('body');
+    const card = document.getElementsByClassName('card');
+
+    bodyTag[0].style.color = '#202D36';
+    bodyTag[0].style.backgroundColor = '#F3F3F3';
+
+    for (let i = 0; i < card.length; i++) {
+        const element = card[i];
+        element.style.backgroundColor = '#fff'
+    }
+
+}
